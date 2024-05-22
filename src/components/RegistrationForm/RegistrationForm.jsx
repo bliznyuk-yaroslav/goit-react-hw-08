36df4588fc605d8e5b69d8ea1960d3c517617bb8
bliznyuk-yaroslav <144949333+bliznyuk-yaroslav@users.noreply.github.com>
2024-05-22 09:52:05 +0300

    edi registr

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -2,6 +2,7 @@ import { Formik, Form, Field } from "formik";
 import { useDispatch } from "react-redux";
 import { register } from "../../redux/auth/operations";
 import css from "./RegistrationForm.module.css";
+import Button from "@mui/material/Button";
 
 export default function RegistrationForm() {
   const dispatch = useDispatch();
@@ -24,17 +25,26 @@ export default function RegistrationForm() {
       <Form className={css.form} autoComplete="off">
         <label className={css.label}>
           Username
-          <Field type="text" name="name" />
+          <Field className={css.input} type="text" name="name" />
         </label>
         <label className={css.label}>
           Email
-          <Field type="email" name="email" />
+          <Field className={css.input} type="email" name="email" />
         </label>
         <label className={css.label}>
           Password
-          <Field type="password" name="password" />
+          <Field className={css.input} type="password" name="password" />
         </label>
-        <button type="submit">Register</button>
+        <Button
+          style={{ width: "200px" }}
+          variant="contained"
+          color="primary"
+          sx={{ mt: 3, mb: 2 }}
+          type="submit"
+          className={css.btn}
+        >
+          Register
+        </Button>
       </Form>
     </Formik>
   );
